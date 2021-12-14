a452b62636d8406f021edeaae03ff2dd51ae00f8
wubeZ <wubezeleke@gmail.com>
2021-12-14 17:09:14 +0300

    added solution for domino piling

---
+++ week 1/Domino piling.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Domino{
+    public static void main (String[]args){
+        Scanner input = new Scanner(System.in);
+        int n= input.nextInt();
+        int m = input.nextInt();
+    int var1 = m*n;
+    int var2 =var1/2;
+    System.out.println(var2);
+
+    }
+}
