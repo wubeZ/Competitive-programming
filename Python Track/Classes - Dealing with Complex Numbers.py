aa877f77619fc93d1ba8eb0754f441f4697b669b
wubeZ <85889061+wubeZ@users.noreply.github.com>
2022-01-10 21:42:09 +0300

    Add files via upload

---
+++ Python Track/Classes - Dealing with Complex Numbers.py
@@ -0,0 +1,40 @@
+import math
+
+class Complex(object):
+    def __init__(self, real, imaginary):
+        self.real = real
+        self.imaginary = imaginary        
+    def __add__(self, no):
+        real = self.real + no.real
+        imaginary = self.imaginary +no.imaginary
+        return Complex(real,imaginary)
+    def __sub__(self, no):
+        real = self.real - no.real
+        imaginary = self.imaginary - no.imaginary
+        return Complex(real,imaginary)
+    def __mul__(self, no):
+        real = (self.real * no.real) - (self.imaginary * no.imaginary)
+        imaginary =(self.imaginary * no.real)+(self.real*no.imaginary)
+        return Complex(real,imaginary)
+    def __truediv__(self, no):
+        div = pow(no.real, 2) + pow(no.imaginary, 2)
+        real =((self.real*no.real) +(self.imaginary *no.imaginary))/div
+        imaginary =((self.imaginary*no.real)-(self.real* no.imaginary))/div
+        return Complex(real, imaginary)
+    def mod(self):
+        return Complex(round(pow((pow(self.real, 2) + pow(self.imaginary, 2)), 0.5), 2), 0)
+    def __str__(self):
+        if self.imaginary == 0:
+            result = "%.2f+0.00i" % (self.real)
+        elif self.real == 0:
+            if self.imaginary >= 0:
+                result = "0.00+%.2fi" % (self.imaginary)
+            else:
+                result = "0.00-%.2fi" % (abs(self.imaginary))
+        elif self.imaginary > 0:
+            result = "%.2f+%.2fi" % (self.real, self.imaginary)
+        else:
+            result = "%.2f-%.2fi" % (self.real, abs(self.imaginary))
+        return result
+
+if __name__ == '__main__':
